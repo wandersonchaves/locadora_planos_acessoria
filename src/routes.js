e0a37801af4b3724e0c8ed54831dcee9534366a4
Wanderson Chaves <wandersonscpibr@gmail.com>
2021-07-05 17:03:38 -0300

    add new routes searchMovie and authUser

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,18 @@ const express = require("express");
 
 const UserController = require("./controllers/UserController");
 const MovieController = require("./controllers/MovieController");
+const ReportMovieController = require("./controllers/ReportMovieController");
 
 const routes = express.Router();
 
 routes.get("/users", UserController.index);
 routes.post("/users", UserController.store);
+routes.post("/auth", UserController.oauth);
 
 routes.get("/users/:user_id/movies", MovieController.index);
 routes.post("/users/:user_id/movies", MovieController.store);
 routes.delete("/users/:user_id/movies", MovieController.delete);
+
+routes.get("/report", ReportMovieController.show);
 
 module.exports = routes;
